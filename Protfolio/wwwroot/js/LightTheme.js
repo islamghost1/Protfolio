6b81083262ee9f955f4a4bb7ba80bfad2f0edf6a
islam devGit <islam.sissaoui@usherbrooke.ca>
2024-07-28 15:22:02 -0400

    ui changes

--- Protfolio/wwwroot/js/LightTheme.js
+++ Protfolio/wwwroot/js/LightTheme.js
@@ -1,4 +1,14 @@
-﻿const checkbox = document.getElementById("checkbox")
+﻿const checkbox = document.getElementById("checkbox");
+const img = document.querySelector(".d-flex img");
+
 checkbox.addEventListener("change", () => {
-    document.body.classList.toggle("dark")
-})
+    document.body.classList.toggle("dark");
+    document.querySelector(".header").classList.toggle("dark");
+
+    // Toggle the image source
+    if (img.src.endsWith("/src/logo-inverted.png")) {
+        img.src = "/src/logo.png";
+    } else {
+        img.src = "/src/logo-inverted.png";
+    }
+});
